60f8e3e74bec985130ea3eb2f006f4f1eebe18f7
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2021-09-15 02:36:47 +0530

    Interface for Application Layer to using sbstractions to communicate to persistamce messaging. infra.

---
+++ CreatorService/Core/Trip.Creator.Application/Contracts/Messaging/IQueuePubliser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trip.Creator.Domain.Entities;
+
+namespace Trip.Creator.Application.Contracts.Messaging
+{
+    public interface IQueuePubliser
+    {
+        public bool InitiateCreationProcessing(Creation creation);
+    }
+}
